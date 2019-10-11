df7fc06912f468958e2a9c56d20e44ac9dd57e9d
Juan C. Jaimes Mdz <jcendersgame@gmail.com>
2019-10-10 19:15:33 -0700

    Update Tarea_2.py

--- Jczerocool/Tarea_2.py
+++ Jczerocool/Tarea_2.py
@@ -1,8 +1,43 @@
+# Ejercicios
+#
+# iris.data
+#
+# Lee desde archivo el conjunto de datos iris.data utiliza el método read_csv().
+#
+# Nombra a los atributos como se especifica en la información del conjunto de datos:
+#
+#   Attribute Information:
+#    1. sepal length in cm
+#    2. sepal width in cm
+#    3. petal length in cm
+#    4. petal width in cm
+#    5. class:
+#       -- Iris Setosa
+#       -- Iris Versicolour
+#       -- Iris Virginica
+# Haz una gráfica con los datos como se hizo en el ejercicio de introducción utilizando el método plot().
+#
+# Imprime cual es el ancho y largo promedio del sépalo y pétalo.
 
+
+# importe de librerias necesarias para la ejecucion
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+
+# Lee desde archivo el conjunto de datos iris.data utiliza el método read_csv().
+#  Nombra a los atributos como se especifica en la información del conjunto de datos:
+#
+#   Attribute Information:
+#    1. sepal length in cm
+#    2. sepal width in cm
+#    3. petal length in cm
+#    4. petal width in cm
+#    5. class:
+#       -- Iris Setosa
+#       -- Iris Versicolour
+#       -- Iris Virginica
 
 
 # cambio de nombre en los headers, ademas de separaciones por coma
@@ -12,17 +47,15 @@ df2 = pd.read_csv('iris.data', sep=',', header=None,
 # tipo de dato contenido en el dataset
 df2.dtypes
 
+# descripcion de los datos contenidos en el dataset
 df2.describe()
 
-# df2['sepal length in cm'].value_counts().plot(kind='bar')
+# Haz una gráfica con los datos como se hizo en el ejercicio de
+# introducción utilizando el método plot().
 df2['class'].value_counts().plot(kind='pie')
 plt.show()
 
-df2['sepal length in cm'].value_counts()
-
-# df2.plot.scatter(x='sepal length in cm', y='sepal width in cm',c='petal length in cm',cmap='viridis');
-# plt.show()
-
+# Imprime cual es el ancho y largo promedio del sépalo y pétalo.
 df2['sepal length in cm'].mean()
 
 df2['sepal width in cm'].mean()
